08f0ac87943d4a844242884f2ed73ce32be7dccd
BoSSBINHDINH <Anhbuiboyboss@gmail.com>
2026-07-04 19:35:35 +0700

    index.js
    Updated bot connection details and server address.

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ function startBot() {
   console.log("Đang khởi động bot...")
 
   bot = mineflayer.createBot({
-    host: "darkblademc.falix.dev",
-    port: 31985,
+    host: "khovl.progamer.me",
+    port: 48932,
     username: "MeMayBeo",
     version: "1.20.1"
   })
